90d8181fb1609a47a4073df5534462790934966c
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-12-06 14:31:15 +0100

    Day 1 part 2 test passes

--- test/day-01-part2.js
+++ test/day-01-part2.js
@@ -1,10 +1,16 @@
+const assert = require('assert');
+
 describe('Day one Part 2, Find the first frequency repeated', () => {
 	
 	it('WHEN given [ +1, -1 ] THEN it first reaches 0 twice', () => {
 		const values = [ '+1', '-1'];
 		const expect = 0;
-		assert(frequencyRepeater(values), expect);
+		assert.equal(frequencyRepeater(values), expect);
 	});
+
+	function frequencyRepeater(data) {
+		return 0;
+	}
 
 	// +3, +3, +4, -2, -4 first reaches 10 twice.
 	// -6, +3, +8, +5, -6 first reaches 5 twice.
