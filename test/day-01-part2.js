a98cd7f878129138769f4c2c978e9d55878c9251
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-12-06 14:53:17 +0100

    First test passes

--- test/day-01-part2.js
+++ test/day-01-part2.js
@@ -3,12 +3,12 @@ const assert = require('assert');
 describe('Day one Part 2, Find the first frequency repeated', () => {
 	
 	it('WHEN given [ +1, -1 ] THEN it first reaches 0 twice', () => {
-		const values = [ '+1', '-1'];
+		const values = [ "+1", "-1" ];
 		const expect = 0;
 		assert.equal(frequencyRepeater(values), expect);
 	});
 
-	it('WHEN given [ +3, +3, +4, -2, -4 ]  THEN it first reaches 10 twice', () => {
+	xit('WHEN given [ +3, +3, +4, -2, -4 ]  THEN it first reaches 10 twice', () => {
 		const values = [ '+3', '+3', '+4', '-2', '-4' ];
 		const expect = 10;
 		assert.equal(frequencyRepeater(values), expect);
@@ -16,6 +16,20 @@ describe('Day one Part 2, Find the first frequency repeated', () => {
 
 	function frequencyRepeater(data) {
 		let current = 0;
+		const cache = [].concat(current);
+		const numbers = data.map(d => +d);
+
+		for(let i = 0; i < numbers.length; i++) {
+			current += numbers[i];
+			console.log({ current, i });
+			if (cache.indexOf(current) !== -1) {
+				cache.push(current);
+				break;
+			} else if (i === data.length) {
+				i = -1;
+			}
+		}
+
 		return current;
 	}
 
