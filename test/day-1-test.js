e09bca7e5dfcf33de2a70fa60d08ea7b640bd7a9
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-12-06 11:22:11 +0100

    first test passed

--- test/day-1-test.js
+++ test/day-1-test.js
@@ -3,8 +3,11 @@ const assert = require('assert');
 describe('Day 1 part one, Frequency calulator', () => {
 	it('It should default to zero', () => {
 		const expect = 0;
-		const results = frequencyCalulator();
+		const results = frequencyCalculator();
 		assert.equal(results, expect);
 	});
 
+	function frequencyCalculator() {
+		return 0;
+	}
 });
