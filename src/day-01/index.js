9279cfa2389ab076e12b54581f24453c83de068e
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-12-06 16:01:54 +0100

    Day one done

--- src/day-01/index.js
+++ src/day-01/index.js
@@ -2,9 +2,11 @@
 //
 const data = require('./data');
 const { frequencyCalculator } = require('./part-1')
-
+const frequencyRepeater = require('./part-2');
 const part_one_answer = frequencyCalculator(data);
+const part_two_answer = frequencyRepeater(data);
 
 console.log({
 	part_one_answer,
+	part_two_answer,
 });
