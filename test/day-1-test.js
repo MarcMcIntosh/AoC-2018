a885f0a30189b8187c58dde1212b23183f35f063
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-12-06 11:41:50 +0100

    Forgot to pass values in test

--- test/day-1-test.js
+++ test/day-1-test.js
@@ -8,19 +8,19 @@ describe('Day 1 part one, Frequency calulator', () => {
 	});
 
 	describe('WHEN GIVEN an array of stringifyed signed intergers, THEN return sum of these', () => {
-		xit('WHEN GIVEN [ "+1", "+1", "+1" ] THEN return 3', () => {
+		it('WHEN GIVEN [ "+1", "+1", "+1" ] THEN return 3', () => {
 			const values = [ "+1", "+1", "-2" ];
 			const expect = 0;
 			assert.equal(frequencyCalculator(), expect);
 		});
 		
-		xit('WHEN GIVEN [ "+1", "+1", "-2" ] THEN return 0', () => {
+		it('WHEN GIVEN [ "+1", "+1", "-2" ] THEN return 0', () => {
 			const values = [ "+1", "+1", "-2"];
 			const expect = 0;
 			assert.equal(frequencyCalculator(), expect);
 		});
 
-		xit('WHEN GIVEN it [ "-1", "-2", "-3" ] THEN return -6', () => {    
+		it('WHEN GIVEN it [ "-1", "-2", "-3" ] THEN return -6', () => {    
 			const values = [ "-1", "-2", "-3" ];
 			const expect = -6;
 			assert.equal(frequencyCalculator(), expect);
@@ -31,15 +31,17 @@ describe('Day 1 part one, Frequency calulator', () => {
 		it('WHEN GIVEN a signed intergaer string, it returns a the number of euqal value', () => {
 			const value = "-1";
 			const expect = -1;
-			function numberParser(numberString) {
-				return +numberString;
-			}
 			assert.equal(numberParser(value), expect);
 		});
 	});
+	
+	function numberParser(numberString) {
+		return +numberString;
+	}
 
-	function frequencyCalculator() {
-		return 0;
+	function frequencyCalculator(data) {
+		const numbers = data.map(numberParser);
+		return numbers.reduce((a, b) => a + b, 0);
 	}
 
 });
