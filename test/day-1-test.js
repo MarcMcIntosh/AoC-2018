3f521f5e7ee8fb81821f0f8ef5b8e9b16359ad8f
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-12-06 11:33:53 +0100

    Test failes after adding examples from AoC

--- test/day-1-test.js
+++ test/day-1-test.js
@@ -7,7 +7,28 @@ describe('Day 1 part one, Frequency calulator', () => {
 		assert.equal(results, expect);
 	});
 
+	describe('WHEN GIVEN an array of stringifyed signed intergers, THEN return sum of these', () => {
+		it('WHEN GIVEN [ "+1", "+1", "+1" ] THEN return 3', () => {
+			const values = [ "+1", "+1", "-2" ];
+			const expect = 0;
+			assert.equal(frequencyCalculator(), expect);
+		});
+		
+		it('WHEN GIVEN [ "+1", "+1", "-2" ] THEN return 0', () => {
+			const values = [ "+1", "+1", "-2"];
+			const expect = 0;
+			assert.equal(frequencyCalculator(), expect);
+		});
+
+		it('WHEN GIVEN it [ "-1", "-2", "-3" ] THEN return -6', () => {    
+			const values = [ "-1", "-2", "-3" ];
+			const expect = -6;
+			assert.equal(frequencyCalculator(), expect);
+		});
+	});
+
 	function frequencyCalculator() {
 		return 0;
 	}
+
 });
