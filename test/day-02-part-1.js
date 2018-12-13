be105234975c45539a6dbd46792f5579babe76d0
Marc McIntosh <marcmcintosh1987@gmail.com>
2018-12-13 15:27:39 +0100

    Shared letters passes test

--- test/day-02-part-1.js
+++ test/day-02-part-1.js
@@ -38,8 +38,14 @@ describe('Day 2: part 1', () => {
 			assert.equal(expect, result);
 		});
 
-		function sharedLetters() {
-			return '';
+		function sharedLetters(word1, word2) {
+			let letters = '';
+			for(let i = 0; i < word1.length; i+=1) {
+				if(word1[i] === word2[i]) {
+					letters += word1[i];
+				}
+			}
+			return letters;
 		}
 	});
 
