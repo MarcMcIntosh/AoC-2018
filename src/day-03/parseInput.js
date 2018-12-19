c3f0245dc7abba6924178c604a0d44b7250f4e37
Marc McIntosh <email@marcmcintosh.ninja>
2018-12-19 22:58:36 +0100

    moved parseInput function to src dir

---
+++ src/day-03/parseInput.js
@@ -0,0 +1,28 @@
+function parseInput(plan) {
+
+	const id = /#\d+/.exec(plan)[0];
+	const startCoords = /\d+,\d+/.exec(plan)[0].split(',').map(d => +d);
+
+	const [ startX, startY ] = startCoords;
+	const coords = [];
+
+	// repitition here
+	const [ endX, endY ] = /\d+x\d+/.exec(plan)[0].split('x').map(d => +d);
+
+	for (let x = 0; x < endX; x+=1) {
+		
+		const currentX = startX + x;
+
+		for(let y = 0; y < endY; y+=1) {
+				
+			const currentY = startY + y;
+			const XYstring = currentX + ',' + currentY;
+				
+			coords.push(XYstring);
+		}
+	}
+
+	return { id, coords };
+};
+
+module.exports = parseInput;
