369928d88c97b790893a6add964b1e31a9a31db1
Marc McIntosh <email@marcmcintosh.ninja>
2018-12-20 17:26:34 +0100

    Day 3 solved

--- src/day-03/index.js
+++ src/day-03/index.js
@@ -1,7 +1,11 @@
 const data = require('./data');
 const numberOfConflicts = require('./numberOfConflicts');
+const nonOverLapping = require('./nonOverLapping');
 
 const partOneAnswer = numberOfConflicts(data);
+const partTwoAnswer = nonOverLapping(data);
+
 
 console.log('Day three');
 console.log('Part 1, number of conflicts: ', partOneAnswer); 
+console.log('Part 2, non overlapping: ', partTwoAnswer);
