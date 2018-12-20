93e916d5c5bcf09d68e9609097a7695a7d9d75df
Marc McIntosh <email@marcmcintosh.ninja>
2018-12-20 16:39:11 +0100

    Day 3 part one complete

---
+++ src/day-03/index.js
@@ -0,0 +1,7 @@
+const data = require('./data');
+const numberOfConflicts = require('./numberOfConflicts');
+
+const partOneAnswer = numberOfConflicts(data);
+
+console.log('Day three');
+console.log('Part 1, number of conflicts: ', partOneAnswer); 
