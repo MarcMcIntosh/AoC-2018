fe0f8fc9ef46fde95220797c2282ac2f92686798
Marc McIntosh <email@marcmcintosh.ninja>
2018-12-20 17:18:53 +0100

    Non over lapping test passes

--- test/day-03.js
+++ test/day-03.js
@@ -231,7 +231,25 @@ describe('Day 3: part 2, find the id of the only plan that does not overlap', ()
 		assert.equal(findNonOverLapping(values), expect);
 
 		function findNonOverLapping(plans) {
-			return 3;
+			const allCoords = getAllCoords(plans);
+			
+			const hashMap = coordsHashMap(allCoords);
+			const allPlans = plans.map(parseInput);
+
+			const nonOverLapping = allPlans.filter((plan) => {
+				let good = true;
+				for(let i = 0; i < plan.coords.length && true; i+=1) {
+					const coord = plan.coords[i];
+					if (hashMap[coord] > 1) { 
+						good = false;
+					}
+				}
+				return good;
+			});
+
+
+			const [ bestPlan ] = nonOverLapping;
+			return bestPlan.id;
 		}
 	});
 
