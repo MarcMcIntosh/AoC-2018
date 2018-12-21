db0f3e3b83a7fb294f0c145282bea6a18dc54f54
Marc McIntosh <email@marcmcintosh.ninja>
2018-12-21 15:06:19 +0100

    getGuardNumber test 2 passes

--- test/day-04.js
+++ test/day-04.js
@@ -97,8 +97,13 @@ describe('Day 04 Part 1', () => {
 		});
 
 
-		function getGuardNumber() {
-			return 10;
+		function getGuardNumber(input) {
+			const regex = /#\d+/;
+			const match = regex.exec(input);
+			if (match === null) { return match; }
+			const numberWithLeadingHash = match[0];
+			const numberAsString = numberWithLeadingHash.substr(1);
+			return parseInt(numberAsString);
 		}
 		 
 	});
