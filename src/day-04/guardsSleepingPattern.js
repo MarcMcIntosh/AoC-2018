f3e6787e2abcdb176bf4a0785736959ece15c19d
Marc McIntosh <email@marcmcintosh.ninja>
2018-12-22 00:16:58 +0100

    Fixed iteration issues

--- src/day-04/guardsSleepingPattern.js
+++ src/day-04/guardsSleepingPattern.js
@@ -2,17 +2,21 @@ function guardsSleepingPattern(arrayOfShifts) {
 	const hashMap = {};
 
 	for(let shiftIndex = 0; shiftIndex < arrayOfShifts.length; shiftIndex += 1) {
-		const { id, sleeps: [ start, end ] } = arrayOfShifts[shiftIndex];
-		
+		const { id, sleeps } = arrayOfShifts[shiftIndex];
+
 		if(!hashMap[id]) {
 			hashMap[id] = {}
 		}
-
-		for(let minute = start; minute < end; minute += 1) {
-			if(hashMap[id][minute]) {
-				hashMap[id][minute] += 1;
-			} else {
-				hashMap[id][minute] = 1;
+		
+		for(let slept = 0; slept < sleeps.length; slept += 1) {
+			const [ start, end ] = sleeps[slept];
+			
+			for(let minute = start; minute < end; minute += 1) {
+				if(hashMap[id][minute]) {
+					hashMap[id][minute] += 1;
+				} else {
+					hashMap[id][minute] = 1;
+				}
 			}
 		}
 	}
