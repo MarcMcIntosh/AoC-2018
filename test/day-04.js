9f5a8a592eed491acc3908dc676e7b5ef31c6ab9
Marc McIntosh <email@marcmcintosh.ninja>
2018-12-22 00:16:40 +0100

    Can sum the total sleep per guard

--- test/day-04.js
+++ test/day-04.js
@@ -254,7 +254,13 @@ describe('Day 04 Part 1', () => {
 
 		});
 
-		function totalSleepForEach() {}
+		function totalSleepForEach(hashMap) {
+			return Object.keys(hashMap).map((id) => {
+				const minutes = Object.values(hashMap[id]);
+				const total = minutes.reduce((a, b) => a + b, 0);
+				return { id, total };
+			});
+		}
 
 	});
 
