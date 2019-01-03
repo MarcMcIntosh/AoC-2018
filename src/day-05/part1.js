36a79b844893119689e35ebb4bf2371af7e40042
Marc McIntosh <email@marcmcintosh.ninja>
2019-01-03 17:20:08 +0100

    part 1 solution moved to src

---
+++ src/day-05/part1.js
@@ -0,0 +1,14 @@
+function part1(str) {
+	
+	let curr = str;
+	let next = filterCasePairs(str);
+
+	while(curr !== next) {
+		curr = next;
+		next = filterCasePairs(curr);
+	}
+
+	return curr;
+};
+
+module.exports = part1;
