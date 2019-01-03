0273be1954ab1b1b178560074b8f20d35904bfa3
Marc McIntosh <email@marcmcintosh.ninja>
2019-01-03 20:42:22 +0100

    Day 05 part 2 solved: 5330

--- src/day-05/index.js
+++ src/day-05/index.js
@@ -1,7 +1,11 @@
 const part1 = require('./part1');
+const part2 = require('./part2')
 const data = require('./data');
 
+
 const PART_1_SOLUTION = part1(data);
+const PART_2_SOLUTION = part2(data);
 
-console.log('Day 05 Part One\n', PART_1_SOLUTION);
+console.log('Day 05 Part One:\t', PART_1_SOLUTION);
 
+console.log('Day 05 part Two:\t', PART_2_SOLUTION);
